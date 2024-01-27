d762380c3ab8420d2314252e623375027dbcb237
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2024-01-27 09:48:14 +0100

    feat: new entity ProductImage

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -2,12 +2,13 @@ import { Module } from '@nestjs/common';
 import { ProductsService } from './products.service';
 import { ProductsController } from './products.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ProductImage } from './entities/product-image.entity';
 import { Product } from './entities/product.entity';
 import { CommonModule } from 'src/common/common.module';
 
 @Module({
   controllers: [ProductsController],
   providers: [ProductsService],
-  imports: [TypeOrmModule.forFeature([Product]), CommonModule],
+  imports: [TypeOrmModule.forFeature([Product, ProductImage]), CommonModule],
 })
 export class ProductsModule {}
